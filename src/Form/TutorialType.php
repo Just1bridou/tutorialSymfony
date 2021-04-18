251808d34b23a0951dedf6933b8a23b4fd5167d9
corentyndormoy <corentyn.dormoy@gmail.com>
2021-04-18 15:54:06 +0200

    Tutorial creation

--- src/Form/TutorialType.php
+++ src/Form/TutorialType.php
@@ -6,17 +6,37 @@ use App\Entity\Tutorial;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 
 class TutorialType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('title')
-            ->add('content')
-            ->add('isPublished')
-            ->add('isDeleted')
-            ->add('author')
+            ->add('title', TextType::class, [
+                'label' => 'Titre',
+                'attr' => ['class' => 'form-control'],
+            ])
+            ->add('content', TextareaType::class, [
+                'label' => 'Contenu',
+                'attr' => ['class' => 'form-control'],
+            ])
+            ->add('isPublished', ChoiceType::class, [
+                'label' => 'Le publier tout de suite',
+                'choices'  => [
+                    'Oui' => true,
+                    'Non' => false,
+                ],
+                'expanded' => true,
+                'attr' => ['class' => 'form-check form-control'],
+            ])
+            ->add('submit', SubmitType::class, [
+                'label' => 'Enregistrer',
+                'attr' => ['class' => 'btn btn-primary'],
+            ])
         ;
     }
 
