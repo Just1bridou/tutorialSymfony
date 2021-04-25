d7413cdf43989d98e377114c318dde7c49fcc539
corentyndormoy <corentyn.dormoy@gmail.com>
2021-04-25 17:59:36 +0200

    Add Questions CRUD on Tutorial

--- src/Form/TutorialType.php
+++ src/Form/TutorialType.php
@@ -4,11 +4,13 @@ namespace App\Form;
 
 use App\Entity\Tutorial;
 use App\Entity\Category;
+use App\Form\QuestionType;
 use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\Extension\Core\Type\CollectionType;
 use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
@@ -42,6 +44,13 @@ class TutorialType extends AbstractType
                     return $category->getContent();
                 },
                 'attr' => ['class' => 'form-control'],
+            ])
+            ->add('questions', CollectionType::class, [
+                'entry_type' => QuestionType::class,
+                'entry_options' => ['label' => false],
+                'allow_add' => true,
+                'allow_delete' => true,
+                'by_reference' => false,
             ])
             ->add('submit', SubmitType::class, [
                 'label' => 'Enregistrer',
