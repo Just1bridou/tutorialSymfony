fbb35a4247a09278021d6d60551556e90ce34037
Just1bridou <45208166+Just1bridou@users.noreply.github.com>
2021-04-27 18:47:18 +0200

    md js preview

--- migrations/Version20210427161507.php
+++ migrations/Version20210427161507.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20210324190214 extends AbstractMigration
+final class Version20210427161507 extends AbstractMigration
 {
     public function getDescription() : string
     {
@@ -25,6 +25,11 @@ final class Version20210324190214 extends AbstractMigration
         $this->addSql('CREATE TABLE bookmark (id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, learner_id INTEGER NOT NULL, tutorial_id INTEGER NOT NULL, is_bookmarked BOOLEAN NOT NULL)');
         $this->addSql('CREATE INDEX IDX_DA62921D6209CB66 ON bookmark (learner_id)');
         $this->addSql('CREATE INDEX IDX_DA62921D89366B7B ON bookmark (tutorial_id)');
+        $this->addSql('CREATE TABLE category (id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, category_parent_id INTEGER DEFAULT NULL, content VARCHAR(255) NOT NULL)');
+        $this->addSql('CREATE INDEX IDX_64C19C1B51A1840 ON category (category_parent_id)');
+        $this->addSql('CREATE TABLE comment (id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, author_id INTEGER NOT NULL, tutorial_id INTEGER NOT NULL, content VARCHAR(255) NOT NULL)');
+        $this->addSql('CREATE INDEX IDX_9474526CF675F31B ON comment (author_id)');
+        $this->addSql('CREATE INDEX IDX_9474526C89366B7B ON comment (tutorial_id)');
         $this->addSql('CREATE TABLE "like" (id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, learner_id INTEGER NOT NULL, tutorial_id INTEGER NOT NULL, is_liked BOOLEAN DEFAULT NULL)');
         $this->addSql('CREATE INDEX IDX_AC6340B36209CB66 ON "like" (learner_id)');
         $this->addSql('CREATE INDEX IDX_AC6340B389366B7B ON "like" (tutorial_id)');
@@ -33,9 +38,11 @@ final class Version20210324190214 extends AbstractMigration
         $this->addSql('CREATE TABLE score (id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, tutorial_id INTEGER NOT NULL, learner_id INTEGER NOT NULL, score DOUBLE PRECISION NOT NULL)');
         $this->addSql('CREATE INDEX IDX_3299375189366B7B ON score (tutorial_id)');
         $this->addSql('CREATE INDEX IDX_329937516209CB66 ON score (learner_id)');
-        $this->addSql('CREATE TABLE tutorial (id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, author_id INTEGER NOT NULL, title VARCHAR(255) NOT NULL, content CLOB NOT NULL, is_published BOOLEAN NOT NULL, is_deleted BOOLEAN NOT NULL)');
+        $this->addSql('CREATE TABLE tutorial (id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, author_id INTEGER NOT NULL, category_id INTEGER NOT NULL, title VARCHAR(255) NOT NULL, content CLOB NOT NULL, is_published BOOLEAN NOT NULL, is_deleted BOOLEAN NOT NULL, created_at DATETIME NOT NULL, edited_at DATETIME DEFAULT NULL)');
         $this->addSql('CREATE INDEX IDX_C66BFFE9F675F31B ON tutorial (author_id)');
-        $this->addSql('CREATE TABLE "user" (id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, username VARCHAR(255) NOT NULL, email VARCHAR(255) NOT NULL, password VARCHAR(255) NOT NULL, city VARCHAR(255) NOT NULL, zip VARCHAR(15) NOT NULL, address VARCHAR(255) NOT NULL, birthday DATE NOT NULL, is_banned BOOLEAN NOT NULL)');
+        $this->addSql('CREATE INDEX IDX_C66BFFE912469DE2 ON tutorial (category_id)');
+        $this->addSql('CREATE TABLE "user" (id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, username VARCHAR(255) NOT NULL, email VARCHAR(255) NOT NULL, password VARCHAR(255) NOT NULL, city VARCHAR(255) NOT NULL, zip VARCHAR(15) NOT NULL, address VARCHAR(255) NOT NULL, birthday DATE NOT NULL, is_banned BOOLEAN NOT NULL, roles CLOB NOT NULL --(DC2Type:json)
+        , is_verified BOOLEAN NOT NULL)');
     }
 
     public function down(Schema $schema) : void
@@ -43,6 +50,8 @@ final class Version20210324190214 extends AbstractMigration
         // this down() migration is auto-generated, please modify it to your needs
         $this->addSql('DROP TABLE answer');
         $this->addSql('DROP TABLE bookmark');
+        $this->addSql('DROP TABLE category');
+        $this->addSql('DROP TABLE comment');
         $this->addSql('DROP TABLE "like"');
         $this->addSql('DROP TABLE question');
         $this->addSql('DROP TABLE score');
