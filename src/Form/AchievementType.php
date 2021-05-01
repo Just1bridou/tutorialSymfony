1574e4d9316f4d54d93d9efe937089484fbed2e0
corentyndormoy <corentyn.dormoy@gmail.com>
2021-05-01 16:31:44 +0200

    Translated: Achievement

--- src/Form/AchievementType.php
+++ src/Form/AchievementType.php
@@ -5,7 +5,10 @@ namespace App\Form;
 use App\Entity\Achievement;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\Extension\Core\Type\NumberType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
 class AchievementType extends AbstractType
@@ -13,11 +16,19 @@ class AchievementType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('targetField')
+            ->add('targetField', TextType::class, [
+                'label' => false,
+                'attr' => ['class' => 'form-control',
+                            'placeholder'=> 'form.achievement.target_field'],
+            ])
             ->add('mathematicalOperator')
-            ->add('goal')
+            ->add('goal', NumberType::class, [
+                'label' => false,
+                'attr' => ['class' => 'form-control',
+                            'placeholder'=> 'form.achievement.goal'],
+            ])
             ->add('submit', SubmitType::class, [
-                'label' => 'REGISTER',
+                'label' => 'form.achievement.save',
                 'attr' => ['class' => 'btn btn-primary submitFormQuestions'],
             ])
         ;
